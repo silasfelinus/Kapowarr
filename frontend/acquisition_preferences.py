5c6b5e3a58096a7ce5ee2f1d79e4dac1af2054ca
Silas M Knight <silasfelinus@gmail.com>
2026-08-23 15:28:35 -0700

    Expose grab size limits with acquisition settings

--- frontend/acquisition_preferences.py
+++ frontend/acquisition_preferences.py
@@ -6,7 +6,17 @@ from flask import request
 
 from backend.features.acquisition_preferences import (
     get_acquisition_preferences, update_acquisition_preferences)
+from backend.features.grab_size_limits import (GRAB_SIZE_KEYS,
+                                                get_grab_size_limits,
+                                                update_grab_size_limits)
 from frontend.api import api, auth, error_handler, return_api
+
+
+def _current_preferences():
+    return {
+        **get_acquisition_preferences(),
+        **get_grab_size_limits()
+    }
 
 
 @api.route('/settings/acquisition', methods=['GET', 'PUT'])
@@ -14,7 +24,23 @@ from frontend.api import api, auth, error_handler, return_api
 @auth
 def api_acquisition_preferences():
     if request.method == 'GET':
-        return return_api(get_acquisition_preferences())
+        return return_api(_current_preferences())
 
     data: dict = request.get_json() or {}
-    return return_api(update_acquisition_preferences(data))
+    grab_data = {
+        key: value
+        for key, value in data.items()
+        if key in GRAB_SIZE_KEYS
+    }
+    preference_data = {
+        key: value
+        for key, value in data.items()
+        if key not in GRAB_SIZE_KEYS
+    }
+
+    if preference_data:
+        update_acquisition_preferences(preference_data)
+    if grab_data:
+        update_grab_size_limits(grab_data)
+
+    return return_api(_current_preferences())
